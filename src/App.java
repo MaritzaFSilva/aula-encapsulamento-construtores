a67669d90fae8a1dc1f8d10cf7bd6885690362e3
MaritzaFSilva <maritzafer98@gmail.com>
2024-10-04 12:31:20 -0300

    Ajustes aula

--- src/App.java
+++ src/App.java
@@ -1,39 +1,56 @@
 import java.util.Scanner;
 
 public class App {
-    
-    public static void main(String[] args){
 
-       
-        //declaração
+    public static void main(String[] args) {
+
+        // declaração
         Conta minhaConta;
         Conta outraConta;
+        
 
-        //instanciar
-        minhaConta = new Conta();
-        outraConta = new Conta();
-
+        // instanciar
+        minhaConta = new Conta("1234", "34567", "123456789", "Maritza Silva", 100.0);
+        
 
         // O que acontece, se eu fizer um print de minhaConta.getTitular() ??
 
+        System.out.println(minhaConta.getTitular());
 
-        //inicializamos 
-        minhaConta.agencia = "1234";
-        minhaConta.numero = "34567";
-        minhaConta.cpf = "045.777.777-99";
+        // inicializamos
+        minhaConta.setAgencia("1234");
+        minhaConta.setNumero("34567");
+        minhaConta.setCpf("045.777.777-99");
         minhaConta.setTitular("Zé");
         minhaConta.setSaldo(100.0);
 
-        outraConta.agencia = "4567";
-        outraConta.numero = "876543";
-        outraConta.cpf = "333.444.555-00";
-        outraConta.setTitular("Chico");
-        outraConta.setSaldo(50.0);
+
+        /**
+         * EXEMPLO RÁPIDO DE CÓDIGO PARA DEMONSTRAR A IDEIA
+         * QUE PRECISA MELHORAR
+         */
+
+        // for (int i = 0; i < vetorNumero.length; i++) {
+        //     if(vetorNumero[i]){
+        //         vetorNumero[i] = numero;
+        //         return true;
+        //     }         
+        // }
+        // return false;
 
 
-        System.out.println("MinhaConta:\n"+minhaConta.toString());
 
-        System.out.println(outraConta.toString());
+
+
+        // outraConta.agencia = "4567";
+        // outraConta.numero = "876543";
+        // outraConta.cpf = "333.444.555-00";
+        // outraConta.setTitular("Chico");
+        // outraConta.setSaldo(50.0);
+
+        System.out.println("MinhaConta:\n" + minhaConta.toString());
+
+     
 
         minhaConta.sacar(10);
 
@@ -42,8 +59,7 @@ public class App {
         minhaConta.depositar(50);
 
         System.out.println(minhaConta.toString());
-        System.out.println(outraConta.toString());
+      
 
-        
     }
 }
--- src/Conta.java
+++ src/Conta.java
@@ -1,32 +1,82 @@
 public class Conta {
 
     // atributos=variáveis
-    String agencia;
-    String numero;
-    String cpf;
+    private String agencia;
+    private String numero;
+    private String cpf;
     private String titular;
     private double saldo;
 
+
+    public Conta(String agencia, String numero, String cpf, String titular, double saldo) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.cpf = cpf;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public Conta(String agencia, String numero, double saldo, String cpf, String titular) {
+        this.agencia = agencia;
+        this.numero = numero;
+        this.cpf = cpf;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+    
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    
+
+    public Conta(String agencia, String numero) {
+        // this.agencia = agencia;
+        // this.numero = numero;
+        // this.titular = "";
+        // this.saldo = 0;
+        // this.cpf = "";
+
+        this(agencia, numero, "", "", 0);
+    }
 
     // Método que pega o nome do titular
     public String getTitular() {
         return this.titular;
     }
 
-    //Métodos Set
-    public void setTitular(String titular){
+    // Métodos Set
+    public void setTitular(String titular) {
         this.titular = titular;
     }
 
-    public void setSaldo(Double saldo){
+    public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
-
-    public Double getSaldo(){
+    public double getSaldo() {
         return this.saldo;
     }
-
 
     // métodos=função
     void sacar(double valor) {
